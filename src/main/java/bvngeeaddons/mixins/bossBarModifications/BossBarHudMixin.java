b569447b1798797a4a7526051b45955546a77ecb
Bvngee Cord <nystromjp@gmail.com>
2022-04-28 10:05:14 -0700

    some boss bar stuff again

--- src/main/java/bvngeeaddons/mixins/bossBarModifications/BossBarHudMixin.java
+++ src/main/java/bvngeeaddons/mixins/bossBarModifications/BossBarHudMixin.java
@@ -5,35 +5,38 @@ import bvngeeaddons.config.listEntries.BossBarRenderMode;
 import net.minecraft.client.gui.hud.BossBarHud;
 import net.minecraft.client.gui.hud.ClientBossBar;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.LiteralText;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Mixin(BossBarHud.class)
 public class BossBarHudMixin {
 
     @Shadow @Final private Map<UUID, ClientBossBar> bossBars;
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;drawWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I", shift = At.Shift.AFTER))
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;getScaledWidth()I", shift = At.Shift.AFTER))
     private void injection(MatrixStack matrices, CallbackInfo ci) {
+        System.out.println("hi");
+        //do stuff with bossBars here kinda like below but better
+    }
+
+    /*@Redirect(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Map;values()Ljava/util/Collection;", shift = At.Shift.AFTER))
+    private Iterator<?> injected(Collection values) {
+        Map<UUID, ClientBossBar> map = (Map<UUID, ClientBossBar>) values;
+        Map<UUID, ClientBossBar> compactMap = new LinkedHashMap<>();
         if (BvngeeAddonsFeatures.bossBarRenderMode.getOptionListValue() == BossBarRenderMode.COMPACT) {
-
+            Iterator<UUID> keys = map.keySet().iterator();
+            for (int i = 0; i < 3; i++) {
+                UUID uuid = keys.next();
+                compactMap.put(uuid, map.get(uuid));
+            }
         }
-    }
-
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Map;values()Ljava/util/Collection;", shift = At.Shift.AFTER))
-    private Iterator<?> injected(Collection instance) {
-        Map<UUID, ClientBossBar> map = (Map<UUID, ClientBossBar>) instance;
-    }
+        return compactMap.values().iterator();
+    }*/
 
 }
