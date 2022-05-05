952706a2416edbad7365e4a2d82e13af9ba4cbe8
Bvngee Cord <nystromjp@gmail.com>
2022-05-05 10:05:07 -0700

    bossbar modifications refactor is 99% done! just gotta figure out how/when to make it update (wither naming)

--- src/main/java/bvngeeaddons/mixins/bossBarModifications/BossBarHudMixin.java
+++ src/main/java/bvngeeaddons/mixins/bossBarModifications/BossBarHudMixin.java
@@ -18,21 +18,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Mixin(BossBarHud.class)
 public abstract class BossBarHudMixin {
 
     @Shadow @Final private MinecraftClient client;
     @Shadow @Final private static int WIDTH;
-
     private List<ClientBossBar> allBossBars = new ArrayList<>();
-    private Map<BossBar.Color, List<ClientBossBar>> colorToBossBars = new LinkedHashMap<>();
+    private final Map<BossBar.Color, List<ClientBossBar>> colorToBossBars = new LinkedHashMap<>();
     private List<ClientBossBar> renderedBossBars = new ArrayList<>();
 
     private BossBarRenderMode renderMode = (BossBarRenderMode) BvngeeAddonsFeatures.bossBarRenderMode.getOptionListValue();
@@ -40,92 +34,22 @@ public abstract class BossBarHudMixin {
     private boolean separateNamed = BvngeeAddonsFeatures.separateBossBarsWithNames.getBooleanValue();
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Map;values()Ljava/util/Collection;", shift = At.Shift.AFTER))
-    private Iterator<ClientBossBar> compactModeList(Collection<ClientBossBar> values) {
+    private Iterator<ClientBossBar> bossBarListRedirect(Collection<ClientBossBar> values) {
 
         List<ClientBossBar> newList = values.stream().toList();
-        //check if any of the config settings changed OR the list of bossbars changed.
-        //If so, run updateBossBarList
-        if (!newList.equals(allBossBars) || isConfigChanged()) {
+        if (
+                !newList.equals(allBossBars)
+                || newList.stream().map(n -> n.getName().getString()).equals(allBossBars.stream().map(n -> n.getName().getString()))
+                || isConfigChanged()
+        ) {
+            System.out.println("update");
             allBossBars = newList;
             updateBossBarList();
         }
 
         return filterBossBarTypes(renderedBossBars).iterator();
 
-        /*//moved into Update stuff
-        if (BvngeeAddonsFeatures.bossBarRenderMode.getOptionListValue() == BossBarRenderMode.COMPACT) {
-            colorToBossBars = new LinkedHashMap<>();
-            for (ClientBossBar bossBar : values) {
-                colorToBossBars.computeIfAbsent(bossBar.getColor(), k -> new ArrayList<>()).add(bossBar);
-            }
-            return colorToBossBars.values().stream().map(n -> n.get(0)).iterator();
-
-        } else if (BvngeeAddonsFeatures.bossBarRenderMode.getOptionListValue() == BossBarRenderMode.NONE) {
-
-            return BvngeeAddonsFeatures.separateBossBarsWithNames.getBooleanValue() ?
-                    values.stream().filter(this::isNamed).iterator()
-                    : Collections.emptyIterator();
-
-        } else { //Default
-
-            return values.iterator();
-
-        }*/
     }
-
-    //this becomes unnecessary, because I will set the name to render based on the
-    //corresponding element of the Label list straight into the bossbars
-    /*@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ClientBossBar;getName()Lnet/minecraft/text/Text;"))
-    private Text compactModeLabelUnneeded(ClientBossBar bossBar) {
-
-        //moved into Update stuff
-        final boolean separate = BvngeeAddonsFeatures.separateBossBarsWithNames.getBooleanValue();
-        final BossBarRenderMode mode = (BossBarRenderMode) BvngeeAddonsFeatures.bossBarRenderMode.getOptionListValue();
-
-        if (mode == BossBarRenderMode.COMPACT) {
-
-            List<ClientBossBar> bossBars = filterBossBarTypes(colorToBossBars.get(bossBar.getColor()));
-            List<ClientBossBar> unnamedBossBars = new ArrayList<>(), namedBossBars = new ArrayList<>();
-            for (ClientBossBar clientBossBar : bossBars) {
-                if (isNamed(clientBossBar)) {
-                    namedBossBars.add(clientBossBar);
-                } else {
-                    unnamedBossBars.add(clientBossBar);
-                }
-            }
-            System.out.println("unnamed: " + unnamedBossBars.size() + "    named: " + namedBossBars.size());
-            String unnamedType = unnamedBossBars.get(0).getName().getString();
-            if (separate && namedBossBars.size() > 0) {
-                final String separator = "... , ";
-                final int extraLength = client.textRenderer.getWidth(separator.length() + unnamedType);
-                StringBuilder stringBuilder = new StringBuilder();
-                int width = 0;
-                while (width + extraLength <= WIDTH && namedBossBars.size() > 0) {
-                    stringBuilder.append(namedBossBars.get(0).getName().getString()).append(", ");
-                    namedBossBars.remove(0);
-                    width = client.textRenderer.getWidth(stringBuilder.toString());
-                }
-                final boolean cutOff = namedBossBars.size() > 0;
-                final String unnamedLabel = unnamedBossBars.size() > 0 ?
-                        (cutOff ? separator : "") +
-                        unnamedType +
-                        (unnamedBossBars.size() > 1 ? " - x" + unnamedBossBars.size() : "")
-                        : "";
-                return new LiteralText(stringBuilder.append(unnamedLabel).toString());
-            } else {
-                return new LiteralText(unnamedType + (unnamedBossBars.size() > 1 ? " - x" + bossBars.size() : ""));
-            }
-
-        } else if (mode == BossBarRenderMode.NONE) {
-
-            return separate ? bossBar.getName() : new LiteralText("");
-
-        } else { //Default
-
-            return bossBar.getName();
-
-        }
-    }*/
 
     //todo: move to another class for rendering related utils?
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;getScaledWidth()I", shift = At.Shift.BEFORE))
@@ -148,17 +72,14 @@ public abstract class BossBarHudMixin {
     }
 
     private void updateBossBarList() {
-        System.out.println("update");
+
         if (renderMode == BossBarRenderMode.COMPACT) {
 
             colorToBossBars.clear();
             for (ClientBossBar bossBar : allBossBars) {
                 colorToBossBars.computeIfAbsent(bossBar.getColor(), k -> new ArrayList<>()).add(bossBar);
             }
-            //renderedBossBars = colorToBossBars.values().stream().map(n -> compactModeLabel(n.get(0))).toList();
-            colorToBossBars.values().forEach(k -> {
-                if (k.size() > 0) renderedBossBars.add(compactModeLabel(k.get(0)));
-            });
+            renderedBossBars = colorToBossBars.values().stream().map(n -> compactModeLabel(n.get(0))).toList();
 
         } else if (renderMode == BossBarRenderMode.NONE) {
 
@@ -173,33 +94,41 @@ public abstract class BossBarHudMixin {
 
 
     private ClientBossBar compactModeLabel(ClientBossBar bossBar) {
-
         List<ClientBossBar> bossBars = colorToBossBars.get(bossBar.getColor());
         List<ClientBossBar> namedBossBars = new ArrayList<>(), unnamedBossBars = new ArrayList<>();
         bossBars.forEach(k -> {
             if (isNamed(k)) { namedBossBars.add(k); } else { unnamedBossBars.add(k); }
         });
+        LiteralText label;
+        String unnamedType = unnamedBossBars.size() > 0 ? unnamedBossBars.get(0).getName().getString() : bossBars.get(0).getName().getString();
 
-        StringBuilder namedString = new StringBuilder();
-        final String separator = "... , ";
-        final int extraWidth = 50;
-        int width = 0;
-        while (width <= WIDTH - extraWidth) {
-            namedString.append(namedBossBars.get(0).getName().getString()).append(", ");
-            namedBossBars.remove(0);
-            width = client.textRenderer.getWidth(namedString.toString());
+        if (separateNamed) {
+            StringBuilder namedString = new StringBuilder();
+            final String separator = "... , ";
+            final int extraWidth = 50;
+            int width = 0;
+            while (width <= WIDTH - extraWidth && namedBossBars.size() > 0) {
+                namedString.append(namedBossBars.get(0).getName().getString());
+                namedBossBars.remove(0);
+                if (namedBossBars.size() > 0) namedString.append(", ");
+                width = client.textRenderer.getWidth(namedString.toString());
+            }
+            final boolean cutOff = namedBossBars.size() > 0;
+            final String unnamedString = unnamedBossBars.size() > 0 ? (
+                    (cutOff ? separator : ", ") +
+                    (unnamedType) +
+                    (unnamedBossBars.size() > 1 ? " - x" + unnamedBossBars.size() : "")
+                    ) : "";
+            label = new LiteralText(namedString.append(unnamedString).toString());
+        } else {
+            label = new LiteralText(
+                    bossBars.size() > 1 ?
+                            unnamedType + " - x" + bossBars.size()
+                            : unnamedType
+                    );
         }
 
-        final boolean cutOff = namedBossBars.size() > 0;
-        final String unnamedString = unnamedBossBars.size() > 0 ? (
-                        (cutOff ? separator : "") +
-                                //problem here \/ i think
-                        unnamedBossBars.get(0).getName().getString() +
-                        (unnamedBossBars.size() > 1 ? " - x" + unnamedBossBars.size() : "")
-                ) : "";
-
-        bossBar.setName(new LiteralText(namedString.append(unnamedString).toString()));
-        return bossBar;
+        return new ClientBossBar(bossBar.getUuid(), label, bossBar.getPercent(), bossBar.getColor(), bossBar.getStyle(), bossBar.shouldDarkenSky(), bossBar.hasDragonMusic(), bossBar.shouldThickenFog());
 
     }
 
@@ -220,9 +149,9 @@ public abstract class BossBarHudMixin {
 
     private List<ClientBossBar> filterBossBarTypes(List<ClientBossBar> bossBars) {
         if (shownTypes == ShownBossBarTypes.WITHER) {
-            return bossBars.stream().filter(k -> k.getColor() != BossBar.Color.PURPLE).toList();
-        } else if (shownTypes == ShownBossBarTypes.DRAGON) {
             return bossBars.stream().filter(k -> k.getColor() != BossBar.Color.PINK).toList();
+        } else if (shownTypes == ShownBossBarTypes.DRAGON) {
+            return bossBars.stream().filter(k -> k.getColor() != BossBar.Color.PURPLE).toList();
         } else { //Both
             return bossBars;
         }
